e0b1421bc3aafd763e13377d48d559c1ff3c8d6d
Alex Kysov <48977515+pylsar@users.noreply.github.com>
2021-04-24 21:28:23 +0300

    initial

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,33 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    planets: [
+      {
+        id: 1,
+        name: 'earth'
+      },
+      {
+        id: 2,
+        name: 'mars'
+      },
+      {
+        id: 1,
+        name: 'sun'
+      },
+      {
+        id: 1,
+        name: 'jupiter'
+      }
+    ]
   },
   mutations: {
   },
   actions: {
+  },
+  getters: {
+    getPlanets(state){
+      return state.planets;
+    }
   },
   modules: {
   }
---
+++ src/views/Planets.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="planets">
+      <div v-for="planet in getPlanets" :key="planet.id">
+        <h2>{{planet.name}}</h2>
+      </div>
+  </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex';
+
+export default {
+  name: 'Planets',
+  computed: {
+    ...mapGetters(['getPlanets'])
+  }
+}
+</script>
