5aa26b1717a8e91aff6c8a7d99a4c093f1e527d6
pylsar <kysov2006@bk.ru>
2021-04-28 06:37:27 +0300

    make earth three reactive

--- src/components/Earth.vue
+++ src/components/Earth.vue
@@ -1,38 +1,54 @@
 <template>
-  <div class="earth">
+  <div class="earth" id="earth">
   </div>
 </template>
 <script>
 import * as THREE from "three";
 export default{
   name: 'Earth',
-  mounted(){
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.5,
-      1000
-    );
-    camera.position.z = 200;
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth /2, window.innerHeight/2); 
-    this.$el.appendChild(renderer.domElement);
-
-    //Earth
-    const geometry = new THREE.SphereGeometry(100, 40, 40);
-    const material = new THREE.MeshBasicMaterial({
-      map: new THREE.TextureLoader().load('earth.jpg')
-    });
-    const earth = new THREE.Mesh(geometry, material);
-
-    scene.add(earth);
-    animate();
-    function animate() {
-      requestAnimationFrame(animate);
-      earth.rotation.y -= .01;
-      renderer.render(scene, camera);
+  data(){
+    return{
+      camera: null,
+      scene: null,
+      renderer: null,
+      earth: null
     }
+  },
+  methods:{
+    init: function(){
+      const earthDiv = document.getElementById('earth');
+
+      this.scene = new THREE.Scene();
+      this.camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.5,
+        1000
+      );
+      this.camera.position.z = 200;
+      this.renderer = new THREE.WebGLRenderer();
+      this.renderer.setSize(window.innerWidth /2, window.innerHeight/2); 
+      earthDiv.appendChild(this.renderer.domElement);
+
+      //Earth
+      const geometry = new THREE.SphereGeometry(100, 40, 40);
+      const material = new THREE.MeshBasicMaterial({
+        map: new THREE.TextureLoader().load('earth.jpg')
+      });
+      this.earth = new THREE.Mesh(geometry, material);
+
+      this.scene.add(this.earth);
+    },
+    animate: function(){
+      requestAnimationFrame(this.animate);
+      this.earth.rotation.y -= .01;
+      this.renderer.render(this.scene, this.camera);
+    }
+  },
+  mounted(){
+    this.init();
+    this.animate();
+    
   }
 }
 </script>
