046d9f5cafe85cc883293a2e2edef0e627a832a2
pylsar <kysov2006@bk.ru>
2021-05-16 08:13:26 +0300

    styles fixes

--- src/components/Legend.vue
+++ src/components/Legend.vue
@@ -26,12 +26,17 @@ export default{
     flex-direction: column;
     justify-content: space-evenly;
     align-items: flex-start;    
-    height: 200px;
-    width: 150px;
+    width: 80px;
     background: transparent;
+    &__item{
+        width: 100%;
+        height: 25px;
+        display: flex;
+    }
     &__item-name{
-        padding: 10px 20px;
         color: #000;
+        width: 100%;
+        text-transform: capitalize;
     }
     & h3{
         color: #fff;
